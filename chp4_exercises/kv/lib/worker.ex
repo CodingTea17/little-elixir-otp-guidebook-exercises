173103f9a0525c4188bd04cab9d2e00099d83b9d
Dawson Mortenson <dawson.mortenson@gmail.com>
2018-05-15 11:51:19 -0700

    add a very, very basic read/write GenServer implementation

--- chp4_exercises/kv/lib/worker.ex
+++ chp4_exercises/kv/lib/worker.ex
@@ -9,11 +9,32 @@ defmodule KV.Worker do
     GenServer.start_link(__MODULE__, :ok, opts ++ [name: KVW])
   end
 
+  def write(key, values) do
+    GenServer.call(@name, {:write, key, values})
+  end
+
+  def read(key) do
+    GenServer.call(@name, {:read, key})
+  end
+
   # Server
 
   def init(:ok) do
     { :ok, %{} }
   end
 
+  def handle_call({:write, key, values}, _from, kv_list) do
+    kv_list = Map.put(kv_list, key, values)
+    {:reply, "good", kv_list}
+  end
+
+  def handle_call({:read, key}, _from, kv_list) do
+    {:reply, "here it is #{kv_list[key]}", kv_list}
+  end
+
   # Helper
+
+  # def write_to(key, values) do
+  #   IO.puts(kv_list[key])
+  # end
 end
