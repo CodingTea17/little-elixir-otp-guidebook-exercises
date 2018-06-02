a319aa83e16f14d80fb343d959f257896104155a
Dawson Mortenson <dawson.mortenson@gmail.com>
2018-06-01 17:18:18 -0700

    creates a thy worker to worker under thy supervisor. amazing stuff right here.

--- chp5_exercises/thy_supervisor/lib/thy_supervisor.ex
+++ chp5_exercises/thy_supervisor/lib/thy_supervisor.ex
@@ -86,9 +86,36 @@ defmodule ThySupervisor do
     {:reply, state, state}
   end
 
+  def handle_info({:EXIT, from, :normal}, state) do
+    new_state = state |> Map.delete(from)
+    {:noreply, new_state}
+  end
+
   def handle_info({:EXIT, from, :killed}, state) do
     new_state = state |> Map.delete(from)
     {:noreply, new_state}
+  end
+
+  def handle_info({:EXIT, old_pid, _reason}, state) do
+    case Map.fetch(state, old_pid) do
+      {:ok, child_spec} ->
+        case restart_child(old_pid, child_spec) do
+          {:ok, {pid, child_spec}} ->
+            new_state = state
+                          |> Map.delete(old_pid)
+                          |> Map.put(pid, child_spec)
+            {:noreply, new_state}
+          :error ->
+            {:noreply, state}
+        end
+      _ ->
+        {:noreply, state}
+    end
+  end
+
+  def terminate(_reason, state) do
+    terminate_children(state)
+    :ok
   end
 
   #####################
@@ -133,5 +160,13 @@ defmodule ThySupervisor do
       :error ->
         :error
     end
+  end
+
+  defp terminate_children([]) do
+    :ok
+  end
+
+  defp terminate_children(child_specs) do
+    child_specs |> Enum.each(fn {pid, _} -> terminate_child(pid) end)
   end
 end
